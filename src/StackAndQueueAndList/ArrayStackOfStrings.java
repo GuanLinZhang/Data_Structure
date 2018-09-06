651d1f8286a661bcc3922ec2a12791973582b8e2
DennisZhang <zgl565019676@gmail.com>
2018-09-06 22:54:13 +0800

    Add files via upload
    Stack ADT implemented by Array<String>

---
+++ src/StackAndQueueAndList/ArrayStackOfStrings.java
@@ -0,0 +1,59 @@
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ArrayStackOfStrings implements Iterable<String> {
+    private String[] items;
+    private int n;
+
+    public ArrayStackOfStrings(int capacity) {
+        item = new String[capacity];
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public boolean isFull() {
+        return n == item.length;
+    }
+
+    public void push(String item) {
+        if(!isFull()) {
+            items[n++] = item;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    public String pop() {
+        if(!isEmpty()) {
+            return items[--n];
+        } else {
+            throw new EmptyStackException();
+        }
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return new ReverseArrayIterator<>();
+    }
+
+    private class ReverseArrayIterator<String> implements Iterator<String> {
+        private int i = n - 1;
+
+        @Override
+        public boolean hasNext() {
+            return i >= 0;
+        }
+
+        @Override
+        public String next() {
+            if(!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return items[n--];
+        }
+    }
+}
